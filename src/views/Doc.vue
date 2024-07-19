e83bd8b3bfb2d2fe4f6e4d196eaf2afc6de8fada
chenminjia <chenminjia2014@xiaochuankeji.cn>
2024-07-19 15:03:26 +0800

    feat:页面样式添加

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <Topnav class="nav" />
+    <Topnav toggleMenuButtonVisible class="nav" />
     <div class="content">
       <aside v-if="menuVisible">
         <h2>组件列表</h2>
@@ -40,35 +40,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$linear-gradient:linear-gradient(145deg, #e3fffd 0%, #b7e9e6 100%);
+
 .layout {
   display: flex;
   flex-direction: column;
   height: 100vh;
-  > .nav {
+
+  >.nav {
     flex-shrink: 0;
   }
-  > .content {
+
+  >.content {
     flex-grow: 1;
     padding-top: 60px;
     padding-left: 156px;
+
     @media (max-width: 500px) {
-      padding-left: 0; 
+      padding-left: 0;
     }
   }
 }
+
 .content {
   display: flex;
-  > aside {
+
+  >aside {
     flex-shrink: 0;
   }
-  > main {
+
+  >main {
     flex-grow: 1;
     padding: 16px;
     background: white;
   }
 }
+
 aside {
-  background: lightblue;
+  background: $linear-gradient;
   width: 150px;
   padding: 16px;
   position: fixed;
@@ -76,15 +85,18 @@ aside {
   left: 0;
   padding-top: 70px;
   height: 100%;
-  > h2 {
+
+  >h2 {
     margin-bottom: 4px;
   }
-  > ol {
-    > li {
+
+  >ol {
+    >li {
       padding: 4px 0;
     }
   }
 }
+
 main {
   overflow: auto;
 }

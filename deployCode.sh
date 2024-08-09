92f648e19b79bfaddeb7e62d0f7db2387b2aef7e
chenminjia <chenminjia2014@xiaochuankeji.cn>
2024-08-09 12:42:54 +0800

    update

--- deployCode.sh
+++ deployCode.sh
@@ -2,5 +2,5 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:YBFJ/MJ-UI-Code.git &&
+# git remote add origin git@github.com:YBFJ/MJ-UI-Code.git &&
 git push -f -u origin master 
